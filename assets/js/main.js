c0fe246f968f23e924aba3e3fdc567c37fdf5cf7
herbicus <sonofman_x@hotmail.com>
2015-03-30 16:21:46 -0400

    fixed font and positioning issues

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,28 +7,120 @@ var cornerstoneAPI = (function(options) {
 	var shared = {},
 		options = options || {}
 	
-	// STELLAR.JS
-	$('#main').stellar();
+	// STICKY HEADER
+	$('.nav-bar').addClass('original').clone().insertAfter('.nav-bar').addClass('cloned').css('position','fixed').css('top', 0).css('margin-top', 0).css('z-index','500').removeClass('original').hide();
 
-	$.stellar({
-	  // horizontalOffset: -200,
-	  // verticalOffset: -400,
-	  responsive: true,
-	  scrollProperty: 'scroll',
-	  positionProperty: 'position'
+	scrollIntervalID = setInterval(stickIt, 1);
+
+	function stickIt() {
+
+		var orgElementPos = $('.original').offset();
+		
+		orgElementTop = orgElementPos.top;               
+
+		if ($(window).scrollTop() >= (orgElementTop)) {
+			orgElement = $('.original');
+			coordsOrgElement = orgElement.offset();
+			leftOrgElement = coordsOrgElement.left;  
+			widthOrgElement = orgElement.css('width');
+			$('.cloned').css('left',leftOrgElement+'px').css('top',0).css('width',widthOrgElement).show();
+			$('.original').css('visibility','hidden');
+		} else {
+			$('.cloned').hide();
+			$('.original').css('visibility','visible');
+		}
+	}
+	// end sticky header
+
+	// PARALLAX STUFF
+
+	var aboveFoldParallax = new TimelineMax({paused: true});
+				
+	aboveFoldParallax.to(".l-above-fold", 1, {css:{"background-position-y": 100}});
+
+	window.addEventListener("scroll", function(){
+		var parallaxTiming = document.body.scrollTop / 5000;
+
+		$(".l-above-fold").waypoint(function() {
+
+			aboveFoldParallax.seek(parallaxTiming);
+
+		}, { offset: "90%" });
 	});
 
-	// BX-SLIDR
-	$('#l-bxslidr').bxSlider({
-		pager: false,
-		infiniteLoop: false,
-		touchEnabled: true
+	var typefaceParallax = new TimelineMax({paused: true});
+				
+	typefaceParallax.to(".hero-text", 2, {css:{"background-position-y": 3000}});
+
+	window.addEventListener("scroll", function(){
+		var parallaxTiming = document.body.scrollTop / 5000;
+
+		$(".hero-text").waypoint(function() {
+
+			typefaceParallax.seek(parallaxTiming);
+
+		}, { offset: "90%" });
 	});
+
+	var brushParallax = new TimelineMax({paused: true});
+				
+	brushParallax.to(".parallax-brush", 2, {css:{"top": 2000}});
+
+	window.addEventListener("scroll", function(){
+		var parallaxTiming = document.body.scrollTop / 5000;
+
+		$(".parallax-brush").waypoint(function() {
+
+			brushParallax.seek(parallaxTiming);
+
+		}, { offset: "90%" });
+	});
+
+	var bathroomParallax = new TimelineMax({paused: true});
+				
+	bathroomParallax.to(".l-bathroom", 1, {css:{"background-position-y": 100}});
+
+	window.addEventListener("scroll", function(){
+		var parallaxTiming = document.body.scrollTop / 5000;
+
+		$(".l-bathroom").waypoint(function() {
+
+			bathroomParallax.seek(parallaxTiming);
+
+		}, { offset: "90%" });
+	});
+
+	var kitchenParallax = new TimelineMax({paused: true});
+				
+	kitchenParallax.to(".l-kitchen", 2, {css:{"background-position-y": 100}});
+
+	window.addEventListener("scroll", function(){
+		var parallaxTiming = document.body.scrollTop / 5000;
+
+		$(".l-kitchen").waypoint(function() {
+
+			kitchenParallax.seek(parallaxTiming);
+
+		}, { offset: "90%" });
+	});
+
+	var couchParallax = new TimelineMax({paused: true});
+				
+	couchParallax.to(".l-couch", 2, {css:{"background-position-y": 100}});
+
+	window.addEventListener("scroll", function(){
+		var parallaxTiming = document.body.scrollTop / 5000;
+
+		$(".l-couch").waypoint(function() {
+
+			couchParallax.seek(parallaxTiming);
+
+		}, { offset: "90%" });
+	});
+	// end parallax section
 
 	// MOBILE NAV 
-
 	$('#toggle-switch').on("click", function(){
-		console.log("button clicked");
 		$('.mobile-nav').toggleClass('view-toggle');
 	})
 
@@ -102,15 +194,15 @@ var cornerstoneAPI = (function(options) {
 	    }, 2000);
 	});
 
-	$(".l-video-container").click(function(){
-	     window.location=$(this).find("a").attr("href"); 
-	     return false;
-	});
-
 
 	// BOTTOM BEFORE CLOUSURE
 	var init = function() {
-		console.log('cornerstone API works');
+		
+		TweenMax.to(".hero-text" , 0.75, {
+			left: 0,
+			autoAlpha: 1,
+			ease: Back.easeOut
+		});
 
 	};
 
